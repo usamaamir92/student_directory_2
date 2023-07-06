f593ecd283ed3132ca57ed6f057f12097fb9a824
Usama <usama_amir92@hotmail.com>
2023-07-06 14:33:28 +0100

    students 2

--- seeds/student_directory_2.sql
+++ seeds/student_directory_2.sql
@@ -1,3 +1,40 @@
+
+-- As a coach
+-- So I can get to know all students
+-- I want to see a list of students' names.
+
+-- As a coach
+-- So I can get to know all students
+-- I want to see a list of cohorts' names.
+
+-- As a coach
+-- So I can get to know all students
+-- I want to see a list of cohorts' starting dates.
+
+-- As a coach
+-- So I can get to know all students
+-- I want to see a list of students' cohorts.
+
+-- Nouns
+-- students,names,cohorts,starting dates
+
+-- | Record          | Properties     |
+-- |-----------------|----------------|
+-- | cohort          | starting_date  |
+-- | student         | name,cohort    |
+
+-- cohorts
+-- id: SERIAL
+-- name: text
+-- starting_date: text
+
+-- students
+-- id: SERIAL
+-- name: text
+-- cohort_id: int
+
+-- One cohort can have many students hence students contains the foreign key
+
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY,
   name text,
